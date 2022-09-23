19fdcdb42037f07903051382ee8fe40d4ba960f2
manigggg18 <tmanimasih@gmail.com>
2022-09-23 11:49:25 -0700

    commit

--- bp_projects/projects.py
+++ bp_projects/projects.py
@@ -5,19 +5,6 @@ app_projects = Blueprint('projects', __name__,
                 template_folder='templates/bp_projects/')
 
 # connects /kangaroos path to render kangaroos.html
-@app_projects.route('/portfolio/')
-def portfolio():
-    return render_template("portfolio.html")
-
-# connects /kangaroos path to render kangaroos.html
-@app_projects.route('/kangaroos/')
-def kangaroos():
-    return render_template("kangaroos.html")
-
 @app_projects.route('/walruses/')
 def walruses():
     return render_template("walruses.html")
-
-@app_projects.route('/hawkers/')
-def hawkers():
-    return render_template("hawkers.html")
